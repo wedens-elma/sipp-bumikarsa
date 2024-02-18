cdd1003a4cd02c059d83f8bfb761932a8d09796c
wedens-elma <wedens.elma@ui.ac.id>
2024-02-18 15:02:48 +0700

    src: spring project init

---
+++ sipp/src/main/java/propensi/c06/sipp/SippApplication.java
@@ -0,0 +1,13 @@
+package propensi.c06.sipp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SippApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SippApplication.class, args);
+	}
+
+}
---
+++ sipp/src/test/java/propensi/c06/sipp/SippApplicationTests.java
@@ -0,0 +1,13 @@
+package propensi.c06.sipp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SippApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
