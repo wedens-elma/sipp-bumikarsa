0b3b786db1cad82b4fff8259b72060df25e293bc
Caroline Esther <caroline.esther@ui.ac.id>
2024-03-07 21:55:38 +0700

    Pbi-17: backend (done)

--- sipp/src/main/java/propensi/c06/sipp/controller/RencanaController.java
+++ sipp/src/main/java/propensi/c06/sipp/controller/RencanaController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import propensi.c06.sipp.model.LogRencana;
 import propensi.c06.sipp.model.Rencana;
 import propensi.c06.sipp.service.LogRencanaService;
 import propensi.c06.sipp.service.RencanaService;
@@ -20,6 +22,8 @@ public class RencanaController {
     @GetMapping("/rencana")
     public String daftarRencana(Model model) {
         List<Rencana> listRencana = rencanaService.getAllRencana();
+        List<LogRencana> listLogRencana = logRencanaService.getAllLogRencana();
+        model.addAttribute("listLogRencana", listLogRencana);
         model.addAttribute("listRencana", listRencana);
         return "daftar-rencana";
     }
