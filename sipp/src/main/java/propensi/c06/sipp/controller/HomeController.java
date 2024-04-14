a9ead793f831139754b7b168ede61659e3131aea
Caroline Esther <caroline.esther@ui.ac.id>
2024-04-14 11:37:26 +0700

    PBI-48: frontend (fixing views)

--- sipp/src/main/java/propensi/c06/sipp/controller/HomeController.java
+++ sipp/src/main/java/propensi/c06/sipp/controller/HomeController.java
@@ -5,8 +5,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.ui.Model;
 import propensi.c06.sipp.model.UserModel;
+import propensi.c06.sipp.service.PengadaanService;
 import propensi.c06.sipp.service.UserService;
 
 @Controller
@@ -15,9 +16,17 @@ public class HomeController {
     @Autowired
     UserService userService;
 
-    @GetMapping("/") 
-    public String home() {
+    @Autowired
+    PengadaanService pengadaanService;
 
+    @GetMapping("/") 
+    public String home(Model model) {
+        model.addAttribute("countPaymentBelumDibayar", pengadaanService.countPaymentStatus("belum dibayar"));
+        model.addAttribute("countPaymentSudahDibayar", pengadaanService.countPaymentStatus("sudah dibayar"));
+        model.addAttribute("countPaymentSedangDiproses", pengadaanService.countPaymentStatus("sedang diproses"));
+        model.addAttribute("countShipmentSedangDiproses", pengadaanService.countShipmentStatus("sedang diproses"));
+        model.addAttribute("countShipmentSudahSampai", pengadaanService.countShipmentStatus("sudah sampai"));
+        model.addAttribute("countAllPengadaan", pengadaanService.countAllPengadaan());
         return "index.html";
     }
 }
--- sipp/src/main/java/propensi/c06/sipp/service/PengadaanServiceImpl.java
+++ sipp/src/main/java/propensi/c06/sipp/service/PengadaanServiceImpl.java
@@ -118,4 +118,53 @@ public class PengadaanServiceImpl implements PengadaanService {
         return result;
     }
 
+    @Override
+    public Long countPaymentStatus(String status) {
+        List<Pengadaan> listPengadaan = pengadaanDb.findAll();
+        Long count = 0L;
+        int statusInt;
+        
+        if (status.equalsIgnoreCase("belum dibayar")) {
+            statusInt = 0;
+        } else if (status.equalsIgnoreCase("sudah dibayar")) {
+            statusInt = 2;
+        } else {
+            statusInt = 1;
+        }
+
+        for (Pengadaan pengadaan : listPengadaan) {
+            if (pengadaan.getPaymentStatus() == statusInt) {
+                count += 1;
+            }
+        }
+
+        return count;
+    }
+
+    @Override
+    public Long countShipmentStatus(String status) {
+        List<Pengadaan> listPengadaan = pengadaanDb.findAll();
+        Long count = 0L;
+        int statusInt;
+        
+        if (status.equalsIgnoreCase("sedang diproses")) {
+            statusInt = 0;
+        } else {
+            statusInt = 1;
+        }
+
+        for (Pengadaan pengadaan : listPengadaan) {
+            if (pengadaan.getShipmentStatus() == statusInt) {
+                count += 1;
+            }
+        }
+
+        return count;
+    }
+
+    @Override
+    public Long countAllPengadaan() {
+        return pengadaanDb.count();
+    }
+
 }
