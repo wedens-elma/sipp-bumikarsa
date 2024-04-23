dbdb6e07c3dd9a2e3f4f15e13189ff9195f2c37f
rangga <rangga.yudhistira@ui.ac.id>
2024-04-23 16:27:56 +0700

    pbi-19,20,23: revisi sprint-2 (done)

--- sipp/src/main/java/propensi/c06/sipp/dto/request/UpdateBarangRequestDTO.java
+++ sipp/src/main/java/propensi/c06/sipp/dto/request/UpdateBarangRequestDTO.java
@@ -14,6 +14,9 @@ import java.util.UUID;
 @Data
 public class UpdateBarangRequestDTO extends CreateTambahBarangRequestDTO {
     private String kodeBarang;
+    
+    private String dimensiBarang;
+    private String beratBarang;
 
     
     private Integer stokBarang;
